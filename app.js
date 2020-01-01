1772c61e71f78d7749694fe65a94f81dd43d649a
Md Abdulla Al Mamun <mamun.cseian@gmail.com>
2020-01-01 12:29:24 +0600

    Server created with route

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 const express = require('express');
+const app = express();
+const Route = require('./js/route.js');
+
+app.use(express.json());
+
+app.use('campus/user', Route);
--- server.js
+++ server.js
@@ -1,3 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const app = require('./app');
 
+dotenv.config({path: './config.env'});
+
+const DB = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
+);
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(con =>{
+    console.log('Database connected');
+});
+
+const port = process.env.port || 8000
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}...`);
+});
